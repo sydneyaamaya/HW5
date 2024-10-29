1f2dc8833eab854fb7fa1249480dd4f487e98dc3
sydneyaamaya <150975208+sydneyaamaya@users.noreply.github.com>
2024-10-29 19:07:19 +0000

    added comments

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,6 +1,6 @@
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *   SYDNEY AMAYA / SECTION 001
  *
  *   This java file contains the problem solutions of isSubSet, findKthLargest,
  *   and sort2Arrays methods. You should utilize the Java Collection Framework for
@@ -19,7 +19,7 @@ class ProblemSolutions {
      * array B is a subset if array A. Example:
      *      Input: [1,50,55,80,90], [55,90]
      *      Output: true
-     *      Input: [1,50,55,80,90], [55,90, 99]
+     *      Input: [1,50,55,80,90], [55,90,99]
      *      Output: false
      *
      * The solution time complexity must NOT be worse than O(n).
@@ -31,8 +31,9 @@ class ProblemSolutions {
      */
 
     public boolean isSubset(int list1[], int list2[]) {
-
-        // ADD YOU CODE HERE -- DON'T FORGET TO ADD YOR NAME AT TOP OF FILE
+        /**
+         * 1.Use a for loop to populat a hash table with list1
+         */
 
         return false;
     }
